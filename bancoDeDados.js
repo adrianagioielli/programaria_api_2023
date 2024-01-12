a55269cbe7a509dfd11b416a59419026311b67a6
adriana <adrianalves.cp@gmail.com>
2024-01-12 15:19:32 -0300

    Atualização com banco de dados MongoDB e segurança

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados(){
     try{
         console.log("Conexão com banco de dados iniciada...")
-        await mongoose.connect('mongodb+srv://adrianalvescp:dGYCtioH4i0J80RT@clustermulheres.sqh3w1l.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('Conexão com banco de dados realizada!')
     }catch(erro){
         console.log(erro)
--- mulheres.js
+++ mulheres.js
@@ -1,66 +1,81 @@
 const express = require("express")
 const router = express.Router()
-const { v4: uuidv4} = require('uuid')
-const conectaBancoDeDados = require("./bancoDeDados")
-conectaBancoDeDados()
+//const { v4: uuidv4} = require('uuid')
+//const mulheres -> não lembro mais como estava essa const pra chamar nas funcoes dos verbos
 const porta = 3333
 const app = express()
 app.use(express.json())
-
-const mulheres = [
-    {
-        id: '1',
-        nome: "Adriana Gioielli",
-        image: "https://avatars.githubusercontent.com/u/17130402?v=4",
-        minibio: "Desenvolvedora e Kendoka aspirante"
-
-    },
-    {
-        id: '2',
-        nome: "Simara Conceição",
-        imagem: "https://github.com/simaraconceicao.png",
-        minibio: "Desenvolvedora e Instrutora"
-    },
-    {
-        id: '3',
-        nome: "Iana Chan",
-        imagem: "https://bit.ly/3JCXBqP",
-        minibio: "Fundadora da Programaria"
-    },
-    {
-        id: '4',
-        nome: "Nina da Hora",
-        imagem: "https://bit.ly/3FKpFaz",
-        minibio: "Hackear Antirracista"
-    },
-    {
-        id: '5',
-        nome: 'Luana Pimentel',
-        imagem: 'https://bit.ly/3FKpFaz',
-        minibio: 'Senior Staff Software Engineer',
-    }
-]
+const conectaBancoDeDados = require("./bancoDeDados")
+conectaBancoDeDados()
+const Mulher = require('./mulherModel')
+//pacote cors para acesso da api no frontend
+const cors = require('cors')
+app.use(cors())
 
 //GET
-function mostraMulheres(request, response){
-    response.json(mulheres)
+async function mostraMulheres(request, response){
+    try{
+        //a função find() é abstração do BD
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+        response.json(mulheresVindasDoBancoDeDados)
+    }catch(erro){
+        console.log(erro)
+
+    }
 
 }
 
 //POST
-function criaMulher(request, response){
+async function criaMulher(request, response){
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+    try{
+        //save() é a abstração do mongoose para salvar
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    }catch(erro){
+        console.log(erro)
+    }
+
+    /*bloco para criaar mulher sem conexao com banco de dados
     const novaMulher = {
         id: uuidv4(),
         nome: request.body.nome,
         imagem: request.body.imagem,
-        minibio: request.body.minibio,
+        minibio: request.body.minibio
     }
     mulheres.push(novaMulher)
     response.json(mulheres)
+    */
 }
 
 //PATCH
-function corrigeMulher(request, response){
+async function corrigeMulher(request, response){
+    try{
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+        if(request.body.nome){
+            mulherEncontrada.nome = request.body.nome
+        }
+        if(request.body.imagem){
+            mulherEncontrada.imagem = request.body.imagem
+        }
+        if(request.body.minibio){
+            mulherEncontrada.minibio = request.body.minibio
+        }
+        if(request.body.citacao){
+            mulherEncontrada.citacao = request.body.citacao
+        }
+        const mulherAtualizadaNoBD = await mulherEncontrada.save()
+        response.json(mulherAtualizadaNoBD)
+    }catch(erro){
+        consolo.log(erro)
+    }
+
+    /* bloco para corrigir cadastro sem conexão com banco de dados
     function encontraMulher(mulher){
         if(mulher.id === request.params.id){
             return mulher
@@ -76,18 +91,26 @@ function corrigeMulher(request, response){
     if(request.body.minibio){
         mulherEncontrada.minibio = request.body.minibio
     }
-    response.json(mulheres)
+    response.json(mulheres)*/
 }
 
 //DELETE
-function deletaMulher(request, response){
+async function deletaMulher(request, response){
+    try{
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: "Mulher deletada com sucesso!"})
+    }catch(erro){
+        console.log(erro)
+    }
+    
+    /*bloco para deletar sem conexão com banco de dados
     function todasMenosEla(mulher){
         if(mulher.id !== request.params.id){
             return mulher
         }
     }
     const mulheresQueFicam = mulheres.filter(todasMenosEla)
-    response.json(mulheresQueFicam)
+    response.json(mulheresQueFicam)*/
 }
 
 //funcao da porta
